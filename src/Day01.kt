a1ac94d664d2913e7d761dd8e33eed6cfcf0901f
Thomas Manthey <mail@thomas-manthey.de>
2021-12-01 16:05:46 +0100

    Day 1 solved. Ugly.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,50 @@
 fun main() {
+
+    fun getNumIncreases(input: List<List<Integer>>): Int {
+        var count: Int = 0
+        var lastAvg = 0.0f
+        var avg = 0.0f
+        for (index in input.indices) {
+            print(input[index])
+        }
+        return 0
+    }
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var count: Int = 0
+        for (index in input.indices) {
+            if (index > 0) {
+                if (input[index].toInt() > input[index - 1].toInt()) {
+                    count++
+                }
+            }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count: Int = 0
+        val windowSize: Int = 3
+        var lastAvg = 0.0f
+        var avg = 0.0f
+        val inputInt = input.map { it.toInt() }
+        val windows = inputInt.windowed(windowSize)
+        for (window_index in windows.indices) {
+            avg = windows[window_index].sum() / windowSize.toFloat()
+            if (window_index > 0) {
+                if (avg > lastAvg) {
+                    count++
+                }
+            }
+            lastAvg = avg
+        }
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
