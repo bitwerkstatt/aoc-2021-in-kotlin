122830ef43ca6d9541e539e8d4f7c6f6f0cc32d9
Thomas Manthey <mail@thomas-manthey.de>
2021-12-01 17:09:00 +0100

    Day 1 solved. Now in elegant.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,40 +1,24 @@
-fun main() {
+import java.io.File
 
-    fun part1(input: List<String>): Int {
-        var count: Int = 0
-        for (index in input.indices) {
-            if (index > 0) {
-                if (input[index].toInt() > input[index - 1].toInt()) {
-                    count++
-                }
-            }
-        }
-        return count
-    }
+fun readInput(name: String) = File("src", "$name.txt").readLines()
 
-    fun part2(input: List<String>): Int {
-        var count: Int = 0
-        val windowSize: Int = 3
-        var lastAvg = 0.0f
-        var avg = 0.0f
-        val inputInt = input.map { it.toInt() }
-        val windows = inputInt.windowed(windowSize)
-        for (window_index in windows.indices) {
-            avg = windows[window_index].sum() / windowSize.toFloat()
-            if (window_index > 0) {
-                if (avg > lastAvg) {
-                    count++
-                }
-            }
-            lastAvg = avg
-        }
-        return count
-    }
+fun part1(raw_input: List<String>): Int {
+    val input = raw_input.map { it.toInt() }
+    return input
+        .zipWithNext()
+        .count { (prev, next) -> next > prev }
+}
 
-//    // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 1)
+fun part2(raw_input: List<String>): Int {
+    val input = raw_input.map { it.toInt() }
+    val windowSize = 3
+    return input
+        .windowed(windowSize) { it.sum() }
+        .zipWithNext()
+        .count { (prev, next) -> next > prev }
+}
 
+fun main(args: Array<String>) {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
